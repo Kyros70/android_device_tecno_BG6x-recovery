5668e9aa9644d7fe1f38e4c11244a64efa17fb4c
Kyros70 <zaedmasri000999@gmail.com>
2026-09-05 12:49:20 +0300

    BG6x: Fix TEMP path and lower status bar
    added
    TW_Y_OFFSET := 43
    TW_H_OFFSET := -43
    and changed thermal_zone1 to  thermal_zone0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,10 +146,12 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/sprd_backlight/brightness"
 TW_MAX_BRIGHTNESS := 4090
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone1/temp"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone0/temp"
 TW_DEFAULT_BRIGHTNESS := 3900
 TW_USE_LEGACY_BATTERY_SERVICES := true
 TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/charger-manager/power_supply/battery"
+TW_Y_OFFSET := 43
+TW_H_OFFSET := -43
 
 # INCLUDE TWRP CONFIG
 TW_INCLUDE_REPACKTOOLS := true
